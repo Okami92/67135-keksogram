b82480f2f883ea0cbd2dfa044150a3cc87e99ff6
Leonid Lebedev <lix.lebedev@yandex.ru>
2015-11-12 22:44:06 +0300

    Улучшение статистики

--- js/check.js
+++ js/check.js
@@ -1,26 +1,28 @@
-function getMessage(a, b) {
-  if (typeof a == "boolean") {
-    if (a == true) {
-      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+function getMessage(imageType, imageCount) {
+  if (typeof imageType == "boolean") {
+    if (imageType == true) {
+      return "Переданное GIF-изображение анимировано и содержит " + imageCount + " кадров";
     } else {
       return "Переданное GIF-изображение не анимировано";
     }
-  } else if (typeof a == "number") {
-    return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов"
-  } else if (a instanceof Array && b instanceof Array) {
+  } else if (typeof imageType == "number") {
+    return "Переданное SVG-изображение содержит " + imageType + " объектов и " + imageCount * 4 + " аттрибутов"
+  } else if (imageType instanceof Array && imageCount instanceof Array) {
     var square = 0;
-    if (a.length != b.length) {
-      return "Длины массивов не равны!";
+    if (imageType.length != imageCount.length) {
+      return "Невозможно обработать данное изображение, попробуйте загрузить другое";
     }
-    for (var i = 0; i < a.length; i++) {
-      square += a[i] * b[i];
+    for (var i = 0; i < imageType.length; i++) {
+      square += imageType[i] * imageCount[i];
     }
     return "Общая площадь артефактов сжатия: " + square + " пикселей";
-  } else if (a instanceof Array) {
+  } else if (imageType instanceof Array) {
     var sum = 0;
-    for (var i = 0; i < a.length; i++) {
-      sum += a[i];
+    for (var i = 0; i < imageType.length; i++) {
+      sum += imageType[i];
     }
     return "Количество красных точек во всех строчках изображения: " + sum;
+  } else {
+    return "Невозможно обработать данное изображение, попробуйте загрузить другое";
   }
 }
