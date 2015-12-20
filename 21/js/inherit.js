8c5ee0c70dac7e893708b76cea0ee963c5e80e5c
Keks <keks@htmlacademy.ru>
2015-12-20 19:54:54 +0300

    :heavy_check_mark: Сборка #21

---
+++ 21/js/inherit.js
@@ -0,0 +1,26 @@
+// Подключаем, если будем делать module8-task3 доп. задания
+// Тут прописать define и в pictures.js
+
+/**
+ * Наследование через подмену конструктора на пустой конструктор.
+ */
+
+'use strict';
+
+(function() {
+  /**
+   * Принимаем объекты Child и Parent и присваиваем объекту Child
+   * все свойства и методы объекта Parent
+   * @constructor
+   * @param  {Object} child
+   * @param  {Object} parent
+   */
+  var inherit = function(Child, Parent) {
+    /** @constructor */
+    var EmptyConstructor = function() {};
+    EmptyConstructor.prototype = Parent.prototype;
+    Child.prototype = new EmptyConstructor();
+  };
+
+  window.inherit = inherit;
+})();
